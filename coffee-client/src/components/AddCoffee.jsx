e06afdf34b23feddccfab23fc0814632e4270f46
Fariha Promi <farihapromi2611@gmail.com>
2025-05-24 21:46:09 +0600

    Send data from client to server

--- coffee-client/src/components/AddCoffee.jsx
+++ coffee-client/src/components/AddCoffee.jsx
@@ -1,13 +1,53 @@
 import React from 'react'
 
 const AddCoffee = () => {
+    const handleAddCoffee=e=>{
+        e.preventDefault()
+        const form=e.target
+        const name = form.name.value;
+  const quantity = form.quantity.value;
+  const supplier = form.supplier.value;
+  const chef = form.chef.value;
+  const taste = form.taste.value;
+  const category = form.category.value;
+  const details = form.details.value;
+  const photo = form.photo.value;
+
+  const newCoffee = {
+    name,
+    quantity,
+    supplier,
+    chef,
+    taste,
+    category,
+    details,
+    photo,
+  };
+  console.log(newCoffee)
+
+  //send data to server
+  fetch('http://localhost:5000/coffee',{
+    method:'POST',
+    headers:{
+        'content-type':'application/json'
+    },
+    body:JSON.stringify(newCoffee)
+
+  })
+  .then(res=>res.json)
+  .then(data=>{
+    console.log(data)
+  })
+  .catch(error=>console.log(error))
+
+    }
   return (
     <div className='bg-[#F4F3F0] p-8 md:p-16 m-6 md:m-20'>
         <h2 className='text-center p-4 m-4 text-2xl font-bold'>Add Coffee</h2>
         <p className='p-4 m-4 text-xl'>It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.</p>
-      <form action="">
-  <div className="flex gap-4 ml-8 pl-8">
-    <div className="form-control w-full ">
+      <form onSubmit={handleAddCoffee}>
+  <div className="flex gap-4 ">
+    <div className="form-control md:w-1/2 ml-4 ">
       <label className="label">
         <span className="label-text">Coffee Name</span>
       </label>
@@ -21,7 +61,7 @@ const AddCoffee = () => {
       </label>
     </div>
 
-    <div className="form-control w-full">
+    <div className="form-control md:w-1/2">
       <label className="label">
         <span className="label-text">Available Quantity</span>
       </label>
@@ -37,7 +77,7 @@ const AddCoffee = () => {
   </div>
 
   <div className="flex gap-4 mt-4">
-    <div className="form-control w-full">
+    <div className="form-control md:w-1/2">
       <label className="label">
         <span className="label-text">Supplier</span>
       </label>
@@ -51,7 +91,7 @@ const AddCoffee = () => {
       </label>
     </div>
 
-    <div className="form-control w-full">
+    <div className="form-control md:w-1/2">
       <label className="label">
         <span className="label-text">Chef</span>
       </label>
@@ -67,7 +107,7 @@ const AddCoffee = () => {
   </div>
 
   <div className="flex gap-4 mt-4">
-    <div className="form-control w-full">
+    <div className="form-control md:w-1/2">
       <label className="label">
         <span className="label-text">Taste</span>
       </label>
@@ -81,7 +121,7 @@ const AddCoffee = () => {
       </label>
     </div>
 
-    <div className="form-control w-full">
+    <div className="form-control md:w-1/2">
       <label className="label">
         <span className="label-text">Category</span>
       </label>
@@ -126,7 +166,8 @@ const AddCoffee = () => {
 
   <button
     type="submit"
-    className="mt-6 btn bg-[#D2B48C] text-white hover:bg-orange-700 w-full"
+    className="mt-6 btn bg-[#D2B48C] text-white w-full"
+  
   >
     Add Coffee
   </button>
