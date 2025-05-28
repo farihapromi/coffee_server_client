7df794c251132e708eaf4e1198e6d167b70e42bd
Fariha Promi <farihapromi2611@gmail.com>
2025-05-28 20:45:05 +0600

    Add Providers

---
+++ coffee-client/src/Providers/AuthProvider.jsx
@@ -0,0 +1,12 @@
+import React, { createContext } from 'react'
+const AuthContetx=createContext(null)
+
+const AuthProvider = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AuthProvider
